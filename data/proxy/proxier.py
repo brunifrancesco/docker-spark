191773b8dc4c65b088a199e222ffdf1496250029
Francesco Bruni <brunifrancesco02@gmail.com>
2017-01-06 20:16:28 +0100

    Added proxier script to configure reverse proxy

---
+++ data/proxy/proxier.py
@@ -0,0 +1,37 @@
+import docker
+
+client = docker.from_env()
+from quik import Template
+
+temp = Template("""
+  server {
+    listen 80 default_server;
+    #for @worker in @containers:
+      location /worker@worker.id {
+         proxy_set_header X-Real-IP  $remote_addr;
+         proxy_set_header Host $host;
+         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+          proxy_pass http://@worker.ip:8081/;
+      }
+
+      location /static {
+        proxy_set_header X-Real-IP  $remote_addr;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        proxy_pass http://@worker.ip:8081/static;
+      }
+    #end
+}
+""")
+
+containers = map(lambda item: (item['Names'][0], item['NetworkSettings']['Networks']['bridge']['IPAddress']) ,client.containers())
+containers = filter(lambda item: "worker" in item[0], containers)
+containers = list(map(lambda item: item[1], containers))
+containers = zip(range(0, len(containers)), containers)
+containers = map(lambda item: {'id': item[0], 'ip': item[1]}, containers)
+
+conf = temp.render(locals())
+with open('default', 'wb') as out:
+    out.write(conf)
