97367ca97f2f1d0725883389dba4238b3a1955c7
Francesco Bruni <brunifrancesco02@gmail.com>
2017-01-07 20:35:21 +0100

    Fixed nginx file generator when multiple spark worker were deployed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM gettyimages/spark
-RUN pip install jupyter bokeh
+RUN pip install jupyter bokeh pandas
+RUN jupyter nbextension enable --py widgetsnbextension
 ENV PYSPARK_DRIVER_PYTHON jupyter
 ENV PYSPARK_DRIVER_PYTHON_OPTS 'notebook --notebook-dir=/tmp/data/notebooks --ip=0.0.0.0 --port=8888' pyspark
--- data/proxy/proxier.py
+++ data/proxy/proxier.py
@@ -14,15 +14,14 @@ temp = Template("""
 
           proxy_pass http://@worker.ip:8081/;
       }
-
+    #end
       location /static {
         proxy_set_header X-Real-IP  $remote_addr;
         proxy_set_header Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
 
-        proxy_pass http://@worker.ip:8081/static;
+        proxy_pass http://@worker_static_ip:8081/static;
       }
-    #end
 }
 """)
 
@@ -31,6 +30,7 @@ containers = filter(lambda item: "worker" in item[0], containers)
 containers = list(map(lambda item: item[1], containers))
 containers = zip(range(0, len(containers)), containers)
 containers = map(lambda item: {'id': item[0], 'ip': item[1]}, containers)
+worker_static_ip=containers[0]['ip']
 
 conf = temp.render(locals())
 with open('default', 'wb') as out:
