7dd39c899b35e6b7e60538f411740320ba5f875f
Francesco Bruni <bruni@planetek.it>
2017-01-13 10:06:55 +0100

    Spark Session in Flask application

--- data/server/server.py
+++ data/server/server.py
@@ -1,6 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+from pyspark.sql import SparkSession
+print("Creating the SPARK SESSION")
+
+spark = SparkSession.builder \
+     .master("spark://master:7077") \
+     .appName("Vediamo") \
+     .config("spark.mongodb.input.uri", "mongodb://mongodb/measures.measure") \
+     .getOrCreate()
+
+print("SPARK Session created")
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
